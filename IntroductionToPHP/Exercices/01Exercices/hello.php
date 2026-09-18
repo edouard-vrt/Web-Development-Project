9dc6524c11f0da741f6519e3142dee29ec12adad
edouard-vrt <2901edversterren@student.epfc.eu>
2026-09-18 20:13:41 +0200

    Create hello.php

---
+++ IntroductionToPHP/Exercices/01Exercices/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <p>
+            <?php echo "Hello World"; ?>
+        </p>
+    </body>
+</html>
